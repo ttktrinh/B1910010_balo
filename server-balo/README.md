8e4d22fc2e6108d743a1208d73af2c9a832b6871
Trinh Trần <tranthikieutrinh2018@gmail.com>
2023-12-09 02:15:15 +0700

    tranthikieutrinh_B1910010

---
+++ server-balo/README.md
@@ -0,0 +1,3 @@
+# Tran Thi Kieu Trinh B1910010
+
+### Balo Xinh
---
+++ server-balo/data.js
@@ -0,0 +1,40 @@
+export const data = {
+    "products": [
+        {
+            "id": 1,
+            "name": "BALO LAPTOP HaSun 2004",
+            "price": 309000,
+            "salePrice": 245000,
+            "type": "hasun",
+            "image": "https://product.hstatic.net/1000234732/product/untitled-15_434df2ecbed84a3cb7f1616164039b0d_medium.jpg",
+            "rating": 5,
+            "numReviews": 1,
+            "reviews": [
+                {
+                    "name": "Tran Kieu Trinh",
+                    "star": 5,
+                    "comment": "san pham tot",
+                }
+            ]
+        },
+        {
+            "id": 2,
+            "name": "Balo laptop Sakos Morale",
+            "price": 1690000,
+            "salePrice": 1520000,
+            "type": "sakos",
+            "image": "https://sakos.vn/wp-content/uploads/2022/07/Balo-Laptop-SAKOS-MORALE-den-02-768x768.png",
+            "rating": 5,
+            "numReviews": 1,
+            "reviews": [
+                {
+                    "name": "Tran Kieu Trinh",
+                    "star": 5,
+                    "comment": "san pham tot",
+                }
+            ]
+        },
+       
+
+    ]
+}
