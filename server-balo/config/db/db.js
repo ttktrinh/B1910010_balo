8e7476c72d373c75e9345aafb9fcd28b2707fb56
Trinh Trần <tranthikieutrinh2018@gmail.com>
2023-12-09 02:17:03 +0700

    tranthikieutrinh_B1910010

---
+++ server-balo/config/db/db.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+async function connectDB(){
+    
+    const url = 'mongodb+srv://admin:abcd1234@cluster0.syttpp1.mongodb.net/shop?retryWrites=true&w=majority'
+    try {
+        await mongoose.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+        })
+        console.log("connected to db")
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default connectDB;
